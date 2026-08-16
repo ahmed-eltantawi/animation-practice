db2a78035d6b146ae9e0a8dba29e6a1ecacee6fb
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-16 20:54:01 +0300

    Implement TweenAnimationBuilder in TweenAnimationBuilderView for animation display

--- lib/tween_animation_builder/tween_animation_builder_view.dart
+++ lib/tween_animation_builder/tween_animation_builder_view.dart
@@ -6,6 +6,12 @@ class TweenAnimationBuilderView extends StatelessWidget {
   @override
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0, end: 100),
+      duration: const Duration(seconds: 1),
+      builder: (context, value, child) {
+        return SizedBox(width: 100, height: 100, child: Text(value.toString()));
+      },
+    );
   }
 }
