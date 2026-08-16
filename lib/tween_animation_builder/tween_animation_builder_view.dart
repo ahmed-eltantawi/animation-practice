e2309d5db8d4e9d91fdb7e763bc918694d8a4eb8
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-16 20:59:43 +0300

    Fix TweenAnimationBuilder type and simplify text display in TweenAnimationBuilderView

--- lib/tween_animation_builder/tween_animation_builder_view.dart
+++ lib/tween_animation_builder/tween_animation_builder_view.dart
@@ -8,14 +8,11 @@ class TweenAnimationBuilderView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TweenAnimationBuilder<double>(
-          tween: Tween(begin: 0, end: 100),
+        child: TweenAnimationBuilder(
+          tween: IntTween(begin: 0, end: 100),
           duration: const Duration(seconds: 1),
           builder: (context, value, child) {
-            return Text(
-              value.toString().substring(0, 3),
-              style: TextStyle(fontSize: 60),
-            );
+            return Text(value.toString(), style: TextStyle(fontSize: 60));
           },
         ),
       ),
