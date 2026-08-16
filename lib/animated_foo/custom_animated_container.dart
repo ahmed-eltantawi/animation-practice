80ecc4fc839885b780a5f96d52e108a0bd006fba
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-16 19:26:55 +0300

    Refactor CustomAnimatedContainer to use AnimatedContainer for improved performance

--- lib/animated_foo/custom_animated_container.dart
+++ lib/animated_foo/custom_animated_container.dart
@@ -6,19 +6,12 @@ class CustomAnimatedContainer extends StatelessWidget {
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return TweenAnimationBuilder(
+    return AnimatedContainer(
       curve: Curves.easeInOutQuint,
       duration: Duration(seconds: 1),
-      tween: Tween<double>(begin: 100, end: direction),
-      builder: (context, value, child) =>
-          Container(height: direction, width: direction, color: color),
+      height: direction,
+      width: direction,
+      color: color,
     );
   }
-}
-
-class MyClass<T> {
-  final T begging;
-  final T end;
-
-  MyClass({required this.begging, required this.end});
 }
