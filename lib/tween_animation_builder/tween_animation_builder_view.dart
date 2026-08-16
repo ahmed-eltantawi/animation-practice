5d124819b1eabb33175f3ecd989660f1616243b5
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-16 21:31:59 +0300

    Refactor TweenAnimationBuilderView to use StatefulWidget and enhance animation duration and layout

--- lib/tween_animation_builder/tween_animation_builder_view.dart
+++ lib/tween_animation_builder/tween_animation_builder_view.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
-class TweenAnimationBuilderView extends StatelessWidget {
+class TweenAnimationBuilderView extends StatefulWidget {
   const new({super.key});
 
   @override
+  State<TweenAnimationBuilderView> createState() =>
+      _TweenAnimationBuilderViewState();
+}
+
+int animationKey = 0;
+
+class _TweenAnimationBuilderViewState extends State<TweenAnimationBuilderView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TweenAnimationBuilder(
-          tween: IntTween(begin: 0, end: 100),
-          duration: const Duration(seconds: 1),
-          builder: (context, value, child) {
-            return Text(value.toString(), style: TextStyle(fontSize: 60));
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TweenAnimationBuilder(
+              key: ValueKey(animationKey),
+              curve: Curves.easeOutCirc,
+              tween: IntTween(begin: 0, end: 100),
+              duration: const Duration(seconds: 2),
+              builder: (context, value, child) {
+                return Text(value.toString(), style: TextStyle(fontSize: 60));
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  animationKey++;
+                });
+              },
+              child: Text("Click"),
+            ),
+          ],
         ),
       ),
     );
