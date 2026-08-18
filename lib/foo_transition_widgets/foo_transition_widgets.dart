34c3521061d794472016337d97a02abc8e34da33
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-18 16:01:03 +0300

    Refactor FooTransitionWidgets to use StatefulWidget; implement animations for smooth transitions

--- lib/foo_transition_widgets/foo_transition_widgets.dart
+++ lib/foo_transition_widgets/foo_transition_widgets.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
-class FooTransitionWidgets extends StatelessWidget {
+class FooTransitionWidgets extends StatefulWidget {
   const new({super.key});
 
   @override
+  State<FooTransitionWidgets> createState() => _FooTransitionWidgetsState();
+}
+
+class _FooTransitionWidgetsState extends State<FooTransitionWidgets>
+    with SingleTickerProviderStateMixin {
+  late Animation<AlignmentGeometry> _greenAnimation;
+  late Animation<AlignmentGeometry> _redAnimation;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+      reverseDuration: const Duration(seconds: 4),
+    );
+
+    _greenAnimation = Tween<AlignmentGeometry>(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    ).animate(_controller);
+    _redAnimation = Tween<AlignmentGeometry>(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+    ).animate(_controller);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: _stackWidget());
+  }
+
+  Stack _stackWidget() {
+    return Stack(
+      children: [
+        AlignTransition(
+          alignment: _greenAnimation,
+          child: CircleAvatar(radius: 50, backgroundColor: Colors.green),
+        ),
+        AlignTransition(
+          alignment: _redAnimation,
+          child: CircleAvatar(radius: 50, backgroundColor: Colors.red),
+        ),
+      ],
+    );
   }
 }
