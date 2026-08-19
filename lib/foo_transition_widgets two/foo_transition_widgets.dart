3c899731aaf34cc0a87ee6c3e3c2b82884c138e1
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-19 12:15:13 +0300

    Implement FooTransitionWidgets with animations and control buttons; add forward, stop, reverse, reset, and repeat functionalities

---
+++ lib/foo_transition_widgets two/foo_transition_widgets.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class FooTransitionWidgets extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<FooTransitionWidgets> createState() => _FooTransitionWidgetsState();
+}
+
+class _FooTransitionWidgetsState extends State<FooTransitionWidgets>
+    with SingleTickerProviderStateMixin {
+  late Animation<AlignmentGeometry> _greenAnimation;
+  late Animation<AlignmentGeometry> _redAnimation;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+      reverseDuration: const Duration(seconds: 1),
+    );
+
+    _greenAnimation =
+        Tween<AlignmentGeometry>(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ).animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: Curves.bounceIn,
+            reverseCurve: Curves.bounceOut,
+          ),
+        );
+    _redAnimation = Tween<AlignmentGeometry>(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: _stackWidget()),
+          _buttons(),
+          const SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+
+  Stack _stackWidget() {
+    return Stack(
+      children: [
+        AlignTransition(
+          alignment: _greenAnimation,
+          child: CircleAvatar(radius: 50, backgroundColor: Colors.green),
+        ),
+        AlignTransition(
+          alignment: _redAnimation,
+          child: CircleAvatar(radius: 50, backgroundColor: Colors.red),
+        ),
+      ],
+    );
+  }
+
+  Widget _buttons() {
+    return Wrap(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            _controller.forward();
+          },
+          child: const Text("Forward"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _controller.stop();
+          },
+          child: const Text("Stop"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _controller.reverse();
+          },
+          child: const Text("Reverse"),
+        ),
+
+        ElevatedButton(
+          onPressed: () {
+            _controller.reset();
+          },
+          child: const Text("Reset"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _controller.repeat();
+          },
+          child: const Text("Repeat without reverse"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _controller.repeat(reverse: true);
+          },
+          child: const Text("Repeat with reverse"),
+        ),
+      ],
+    );
+  }
+}
